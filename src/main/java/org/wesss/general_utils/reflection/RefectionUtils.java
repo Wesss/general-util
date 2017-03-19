7393ddad01a9d523d8b617d23b02c3615c2fc43c
Wesley <wesleymcox@gmail.com>
2017-03-19 14:17:00 -0700

    reflective invoke that wraps all throwables in a runtime exception

--- src/main/java/org/wesss/general_utils/reflection/RefectionUtils.java
+++ src/main/java/org/wesss/general_utils/reflection/RefectionUtils.java
@@ -1,14 +1,12 @@
 package org.wesss.general_utils.reflection;
 
-import org.junit.internal.runners.model.ReflectiveCallable;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class RefectionUtils {
 
     /**
-     * Invokes the given method, throwing any throws it makes instead of throwing a
+     * Invokes the given method, rethrowing any exceptions caught instead of throwing a
      * {@link InvocationTargetException}
      * @param method method to run
      * @param target object that is to run method
@@ -16,8 +14,27 @@ public class RefectionUtils {
      * @return the returned value of given method
      * @throws Throwable rethrown from given method
      */
-    public static Object invokeUnwrappingThrowables(Method method, Object target, Object... params)
+    public static Object invokeRethrowingThrowables(Method method, Object target, Object... params)
             throws Throwable {
         return new ObjectMirror(target).call(method, params);
+    }
+
+    /**
+     * Invokes the given method, rethrowing any exceptions caught wrapped in a
+     * {@link RuntimeException} instead of throwing a {@link InvocationTargetException}
+     * @param method method to run
+     * @param target object that is to run method
+     * @param params parameters to method call
+     * @return the returned value of given method
+     * @throws RuntimeException containing the exception thrown from given method
+     */
+    public static Object invokeRethrowingInRuntimeException(Method method,
+                                                            Object target,
+                                                            Object... params) {
+        try {
+            return new ObjectMirror(target).call(method, params);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 }
